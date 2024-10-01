90246c3b928b7b3f5bf959388631e05cad6eb726
Munish <muniyaraj2315@gmail.com>
2024-10-01 19:39:52 +0530

    Working on Create Post page and home page customization

---
+++ iOS Journey/CollectionViews/ContentFeedCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  ContentFeedCollectionCell.swift
+//  iOS Journey
+//
+//  Created by MacBook on 01/10/24.
+//
+
+import UIKit
+
+class ContentFeedCollectionCell: UICollectionViewCell {
+    
+}
